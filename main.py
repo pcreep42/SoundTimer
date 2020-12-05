de84b5e8733ad81abca1f9fbca6712f7433468b1
pcreep42 <vladimir.horlach@gmail.com>
2020-12-05 12:41:12 +0200

    3 programs are written for a script

--- main.py
+++ main.py
@@ -9,15 +9,64 @@ def count_n_call(time_qty: int):
     winsound.PlaySound('bell.wav', winsound.SND_NOWAIT)
 
 
-def main():
+def prepare_to_exercise():
     count_n_call(10)
-    print('First iteration started')
-    count_n_call(60)
-    print('Iteration finished\nChange the position')
-    count_n_call(5)
-    print('Second iteration started')
-    count_n_call(60)
-    print('Well done!')
+
+
+def start_iteration(beginning_text: str, iteration_duration: int = 60):
+    print(beginning_text)
+    count_n_call(iteration_duration)
+
+
+def s_c_a_e():
+    prepare_to_exercise()
+    start_iteration('First iteration')
+    start_iteration('5 seconds for rest', 30)
+    start_iteration('Second iteration')
+    start_iteration('5 seconds for rest', 30)
+    start_iteration('Third iteration')
+    start_iteration('5 seconds for rest', 30)
+    start_iteration('Fourth iteration')
+
+
+def superman():
+    prepare_to_exercise()
+    start_iteration('First iteration')
+    start_iteration('5 seconds for rest', 5)
+    start_iteration('Second iteration')
+
+
+def count_half_minute_r(time_for_rest: int = 60):
+    time_for_rest = time_for_rest
+    start_input = input(f'Press Enter to start. Input time to change iteration time. {time_for_rest} seconds is set.  ')
+    if start_input == '':
+        start_iteration(f' -- get some rest for {time_for_rest} seconds -- ', time_for_rest)
+        count_half_minute_r(time_for_rest)
+    elif start_input[0] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        time_for_rest = int(start_input)
+        start_iteration(f' -- get some rest for {time_for_rest} seconds -- ', time_for_rest)
+        count_half_minute_r(time_for_rest)
+    else:
+        question = input('Do you want to leave the program?')
+        if question[0].upper() == 'Y':
+            pass
+        else:
+            start_iteration(f' -- get some rest for {time_for_rest} seconds -- ', time_for_rest)
+            count_half_minute_r(time_for_rest)
+
+
+def main():
+    print("select a program which you'll do:\n"
+          "1. Superman\n"
+          "2. Stick-Cheburashka-Airplane-Eagle\n"
+          "3. One minute breaks")
+    selected_program = input()
+    if selected_program == '1':
+        superman()
+    elif selected_program == '2':
+        s_c_a_e()
+    elif selected_program == '3':
+        count_half_minute_r()
 
 
 if __name__ == '__main__':
